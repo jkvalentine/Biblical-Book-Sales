84c83d5f64082205a6ecf3b7ebde3085e50a54b1
Jarrod Valentine <jkvalentine7695@gmail.com>
2017-01-23 17:51:29 -0800

    update scrape

--- book_sales_scrape.py
+++ book_sales_scrape.py
@@ -3,8 +3,28 @@ import urllib2
 import os
 import codecs
 
+
+# Set environment variables based on .env file if present.  
+#sing this along with a .env file, WHICH IS NOT CHECKED INTO YOUR REPO! 
+#(aka is listed in your .gitignore) will allow you to set environment variables per 
+#project in each project's own .env when working locally.
+if os.path.exists('.env'):
+    print('Detected local .env, importing environment from .env...')
+    for line in open('.env'):
+        var = line.strip().split('=')
+        if len(var) == 2:
+            os.environ[var[0]] = var[1]
+            print "Setting environment variable:", var[0]
+            sys.stdout.flush()
+
+#  Later, you can access variables set in the environment, whether loaded above 
+#from .env or loaded from the actual environment 
+#(as in Heroku or AWS or Domino, etc.) with something like:
+# foo = os.getenv('BAR')
+
+
 wiki = "https://en.wikipedia.org/wiki/List_of_best-selling_books#List_of_best-selling_single-volume_books"
-header = {'User-Agent': WIKIPEDIA_USER} #Use wikipedia user in bash/z-shell profile
+header = {'User-Agent': os.getenv(WIKIPEDIA_USER)} #Use wikipedia user in .env file
 req = urllib2.Request(wiki,headers=header)
 page = urllib2.urlopen(req)
 soup = BeautifulSoup(page, "lxml")
